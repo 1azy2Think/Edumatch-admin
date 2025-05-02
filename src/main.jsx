a0b990346c6f4524f51518527031dd7b03e676e1
Thamzzz <139208893+1azy2Think@users.noreply.github.com>
2025-05-02 19:30:26 +0800

    route name error fix

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense>
-    <BrowserRouter basename='/EduMatch-admin'>
+    <BrowserRouter basename='/Edumatch-admin'>
       <App />
     </BrowserRouter>
   </Suspense>,
