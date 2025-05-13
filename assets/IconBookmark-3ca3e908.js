7225e08b4db6e4271c9683ecea42bbdcf889e4c5
Thamzzz <139208893+1azy2Think@users.noreply.github.com>
2025-05-14 01:28:23 +0800

    Updates

---
+++ assets/IconBookmark-3ca3e908.js
@@ -0,0 +1,1 @@
+import{c as o}from"./createReactComponent-f455f93b.js";var r=o("bookmark","IconBookmark",[["path",{d:"M18 7v14l-6 -4l-6 4v-14a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4z",key:"svg-0"}]]);export{r as I};
--- assets/styled-81c31b6a.js
+++
@@ -1,1 +0,0 @@
-import{ac as t}from"./index-64f04cf6.js";const s=t(),o=s;export{o as s};
